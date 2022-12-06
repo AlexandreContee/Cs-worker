00235e779e88526946daddd833f1f0fc8d9ffdfa
Alexandre Conte <alexandre.conte10@gmail.com>
2022-12-06 07:39:53 -0300

    Commit

---
+++ CourseEx1/Entities/Department.cs
@@ -0,0 +1,14 @@
+﻿namespace CourseEx1.Entities
+{
+    internal class Department
+    {
+        public string Name { get; set; }
+
+        public Department() { }
+
+        public Department(string name) 
+        { 
+            Name = name;
+        }
+    }
+}
